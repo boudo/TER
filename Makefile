1687aacf670e171c0cd690aea5fd9fa14bc017d3
Joran Robin <joran.robin@ens.uvsq.fr>
2019-01-20 18:35:28 +0100

    Init

---
+++ Makefile
@@ -0,0 +1,3 @@
+Tests_prim:Tests_prim.c
+	gcc Tests_prim.c -o Tests_prim -lgmp
+	./Tests_prim
---
+++ Tests_prim.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include <time.h>
+
+
+
+int main()
+{
+	
+
+	return 0;
+}
