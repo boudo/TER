b0feea352932fb263d06a8a316d306be3ef377ea
JoranRobin <joran.robin@ens.uvsq.fr>
2019-02-27 14:44:50 +0100

    ajout critere_euler

--- TER.c
+++ TER.c
@@ -18,28 +18,29 @@
 int main()
 {
 	
-	// int test, test1, test2,compteur=0;
- // 	mpz_t m_test;
- // 	mpz_inits(m_test,NULL);
- // 	for (int i = 1; i < 100000; ++i)
- // 	{
- // 		mpz_set_ui(m_test,i);
+	 int test, test1, test2,compteur=0;
+  	mpz_t m_test;
+  	mpz_inits(m_test,NULL);
+ 	for (int i = 1; i < 10000; ++i)
+  	{
+ 		mpz_set_ui(m_test,i);
  	
- // 		test = Fermat(m_test,10);
- // 		test1 = Miller_Rabin(m_test, 10);
- // 		test2 = solovayStrassen(m_test, 10);
- // 		printf("%d\n", i);
- // 		printf("fermat = %d\n", test);
- // 		printf("miller = %d\n", test1);
- // 		printf("trassen  = %d\n", test2);
- // 		if(test != test1 || test1 != test2 || test != test2){
- // 			printf("merde!!!!!!!!!!!!!!!!!!!!\n");
- // 			compteur++;
- // 		}
+ 		test = Fermat(m_test,25);
+ 		test1 = Miller_Rabin(m_test, 25);
+ 		test2 = solovayStrassen(m_test, 25);
+  		printf("%d\n", i);
+ 		printf("fermat = %d\n", test);
+ 		printf("miller = %d\n", test1);
+ 		printf("trassen  = %d\n", test2);
+ 		if(test != test1 || test1 != test2 || test != test2){
+ 			printf("merde!!!!!!!!!!!!!!!!!!!!\n");
+ 			compteur++;
+ 			return 0;
+ 		}
  		
- // 	}	
- // 	printf("compteur : %d\n", compteur);
- // 	mpz_clears(m_test, NULL);
+ 	}	
+ 	printf("compteur : %d\n", compteur);
+ 	mpz_clears(m_test, NULL);
 
 
 	// mpz_t resultatJ, randomNumber, aTraiter;
@@ -72,14 +73,14 @@ int main()
 	// mpz_clears(res,NULL);
 	// gmp_randclear(state);
 
- 	gmp_printf("\n###################### generation nbr premier ######################\n\n");
- 	float temps;
-    clock_t t1, t2;
- 	t1 = clock();
-	mesureTempsFichier("mesureTemps/mesure.txt",20,1024);
-	t2 = clock();
-	temps = (float)(t2-t1)/CLOCKS_PER_SEC;
-    printf("temp : %f\n", temps);
+ 	//~ gmp_printf("\n###################### generation nbr premier ######################\n\n");
+ 	//~ float temps;
+    //~ clock_t t1, t2;
+ 	//~ t1 = clock();
+	//~ mesureTempsFichier("mesureTemps/mesure.txt",10,1024);
+	//~ t2 = clock();
+	//~ temps = (float)(t2-t1)/CLOCKS_PER_SEC;
+    //~ printf("temp : %f\n", temps);
 	
 
 	// int moi,lui;
--- fonctions/fonctions.c
+++ fonctions/fonctions.c
@@ -512,7 +512,8 @@ void temoinMiller(mpz_t res, mpz_t a, mpz_t n)
  * 	\param a : a est un résidu quadratique ou non de b
  * 	\param b : b est un residu quadratique ou non de a ?
  */
-int jacobiSymbol(mpz_t a, mpz_t b) {
+int jacobiSymbol(mpz_t a, mpz_t b) 
+{
 	mpz_t tmpa,tmpb,tmp,tmp2;
 	int t;
 	mpz_inits(tmpa,tmpb,tmp,tmp2,NULL);
@@ -548,4 +549,26 @@ int jacobiSymbol(mpz_t a, mpz_t b) {
 		mpz_clears(tmp,tmp2,tmpa,tmpb,NULL);
 		return 0;
 	}
+}
+
+/*! \fn critere_euler(mpz_t res,mpz_t random,mpz_t expo,mpz_t n)
+ * 	\brief Fonction permettant de calculer le Symbole de jacobi (a/p) et de determiner si p divise a ou pas puis si a est un résidu quadratique modulo p ou non
+ * 	\param res : On renvoie le resultat.
+ * 	\param random : random est un nombre aléatoire
+ * 	\param expo : expo est un exposant
+ * 	\param aTraiter : aTraiter est le nombre à traiter
+ */
+void critere_euler(mpz_t res,mpz_t random,mpz_t aTraiter)
+{
+		mpz_t aTraiterMoins1,r,expo;
+		mpz_inits(aTraiterMoins1,r,expo,NULL);
+		
+		mpz_sub_ui(aTraiterMoins1,aTraiter,1);
+		
+		mpz_cdiv_q_ui(expo,aTraiterMoins1,2);
+		mpz_mod_ui(r, aTraiterMoins1, 2);
+		mpz_sub(expo,expo,r);
+		squareAndMultiply(res,random,expo,aTraiter);
+		
+		mpz_clears(aTraiterMoins1,r,expo,NULL);	
 }
